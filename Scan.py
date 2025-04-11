99e584c803e8ad792c899668e69f48d20e09938d
SwapnaSikder <159037429+SwapnaSikder@users.noreply.github.com>
2025-04-11 10:43:24 +0600

    Add files via upload

---
+++ Scan.py
@@ -0,0 +1,51 @@
+def scan_disk_scheduling(requests, head, direction, disk_size):
+    requests.sort()
+    seek_sequence = []
+    seek_count = 0
+    left = [i for i in requests if i < head]
+    right = [i for i in requests if i >= head]
+
+    if direction == "left":
+        # Move left first
+        for i in reversed(left):
+            seek_sequence.append(i)
+            seek_count += abs(head - i)
+            head = i
+        # Then move to beginning (if not already there)
+        if left:
+            seek_count += head  # move to 0
+            head = 0
+        # Then move right
+        for i in right:
+            seek_sequence.append(i)
+            seek_count += abs(head - i)
+            head = i
+    elif direction == "right":
+        # Move right first
+        for i in right:
+            seek_sequence.append(i)
+            seek_count += abs(head - i)
+            head = i
+        # Then move to end
+        if right:
+            seek_count += (disk_size - 1 - head)
+            head = disk_size - 1
+        # Then move left
+        for i in reversed(left):
+            seek_sequence.append(i)
+            seek_count += abs(head - i)
+            head = i
+
+    return seek_sequence, seek_count
+
+
+# Example input
+requests = [98, 183, 37, 122, 14, 124, 65, 67]
+head = 53
+direction = "right"
+disk_size = 200
+
+sequence, total_seek = scan_disk_scheduling(requests, head, direction, disk_size)
+
+print("Seek Sequence:", sequence)
+print("Total number of seek operations::", total_seek)
